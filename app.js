27d1a35d75566dc77ba3077ae5872e7cee151fc0
Max <1998max7362@mail.ru>
2023-09-20 19:19:03 +0300

    начало

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ import { errors } from 'celebrate';
 import { mongoose } from 'mongoose';
 import cookieParser from 'cookie-parser';
 import corsAllow from './src/middlewares/CORS.js';
-// import errorHandler from './src/middlewares/Errors/errorHandler.js';
+import errorHandler from './src/middlewares/Errors/errorHandler.js';
 // import cardRouter from './src/routes/cardRoutes.js';
 // import userRouter from './src/routes/userRoutes.js';
 import limiter from './src/middlewares/rateLimit.js';
 // import authRouter from './src/routes/authRoutes.js';
-// import auth from './src/middlewares/auth.js';
-// import WrongRouteError from './src/middlewares/Errors/customErrors/WrongRouteError.js';
+import auth from './src/middlewares/auth.js';
+import WrongRouteError from './src/middlewares/Errors/customErrors/WrongRouteError.js';
 import { requestLogger, errorLogger } from './src/middlewares/logger.js';
 
-const { PORT = 3000, MONGODB_URL = 'mongodb://0.0.0.0:27017/mestodb ' } = process.env;
+const { PORT = 3000, MONGODB_URL = 'mongodb://0.0.0.0:27017/bitfilmsdb ' } = process.env;
 
 try {
   await mongoose.connect(MONGODB_URL);
@@ -36,9 +36,9 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-app.use('/', authRouter);
-app.use('/users', auth, userRouter);
-app.use('/cards', auth, cardRouter);
+// app.use('/', authRouter);
+// app.use('/users', auth, userRouter);
+// app.use('/cards', auth, cardRouter);
 app.use('*', () => {
   throw new WrongRouteError();
 });
