ce8294f646fbff09436029e2cfda3c1a1082ef11
Max <1998max7362@mail.ru>
2023-09-22 16:55:36 +0300

    убрал лишнее

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-// import getUsers from '../controllers/Users/getUsers.js';
-// import getUserById from '../controllers/Users/getUserById.js';
-// import updateAvatar from '../controllers/Users/Me/updateAvatar.js';
 import updateMe from '../controllers/Users/Me/updateMe.js';
 import getMe from '../controllers/Users/Me/getMe.js';
 import {
   authorizedUserSchema,
   patchUserSchema,
-  // patchAvatarSchema,
-  // userIdSchema,
 } from '../ValidationSchemas/ValidationSchemas.js';
 
 const userRouter = Router();
